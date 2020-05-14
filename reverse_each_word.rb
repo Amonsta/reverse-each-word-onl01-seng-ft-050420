f7a8e03f1b6093e6866a47ca276e8311524d94a9
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-14 19:03:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,5 @@ end
 
 # can't call collect on a string so I split the string before calling it 
 # needed to wait for the loop to finish before calling my .join b/c 
+
+#Because your collect is returning each reversed word at a time, and the bigger "collect" picture is an array full of those individual reversed words - so thats what your method was returning, that array. So we sort of intercepted that array and said hey before you return it make sure its back to one string
